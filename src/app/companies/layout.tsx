1cc1d65651af5344a0397a0dfeda7c125fe53801
Andrii Hutnyk <andreigutnik@gmail.com>
2024-02-25 20:57:17 +0100

    Add layout

---
+++ src/app/companies/layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+import Sidebar from "../components/sidebar";
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Sidebar />
+      <div>{children}</div>
+    </>
+  );
+}
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
-  return <div></div>;
+  return (
+    <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
+      <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
+        <Image width={122} height={24} src="icons/logo.svg" alt="Logo" />
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+        <button>
+          <Image
+            width={22}
+            height={22}
+            src="icons/briefcase.svg"
+            alt="Companies icon"
+          />
+          <span className="text-neutral-50 text-base font-medium leading-normal">
+            Exit
+          </span>
+        </button>
+      </div>
+    </aside>
+  );
 }
