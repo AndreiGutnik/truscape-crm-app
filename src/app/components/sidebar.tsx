6e0530a52e6d22693d152b5bd57009ccc1286bec
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-02 22:15:37 +0100

    Add sidebar

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,27 +1,59 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import SidebarItem from "@/app/components/sidebar-item";
+import { usePathname, useRouter } from "next/navigation";
 
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleExitClick = () => {
+    router.push("/");
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
-        <Image width={122} height={24} src="icons/logo.svg" alt="Logo" />
-        <ul>
-          <li></li>
-          <li></li>
+        <Image
+          className="py-8 mb-11 mx-auto"
+          width={122}
+          height={25}
+          src="/icons/logo.svg"
+          alt="logo"
+        />
+        <ul className="space-y-7">
+          <SidebarItem
+            current={pathname === "/dashboard"}
+            pathname="/dashboard"
+            src="/icons/squares.svg"
+            alt="dashboard icon"
+          >
+            Dashboard
+          </SidebarItem>
+          <SidebarItem
+            current={pathname === "/companies"}
+            pathname="/companies"
+            src="/icons/briefcase.svg"
+            alt="companies icon"
+          >
+            Companies
+          </SidebarItem>
         </ul>
-        <button>
+        <button
+          className="flex items-center gap-2 mb-10 mt-auto mx-auto"
+          onClick={handleExitClick}
+        >
           <Image
-            width={22}
-            height={22}
-            src="icons/briefcase.svg"
-            alt="Companies icon"
+            width={18}
+            height={18}
+            src="/icons/arrow-left-on-rectangle.svg"
+            alt="exit icon"
           />
-          <span className="text-neutral-50 text-base font-medium leading-normal">
-            Exit
-          </span>
+          <span className="font-medium text-white">Exit</span>
         </button>
       </div>
     </aside>
